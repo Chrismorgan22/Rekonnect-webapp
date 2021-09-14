61eafe428a2ca3298ef1cda9f2fbfe01305f7662
LeenaPatel96 <leenap.ddalvin@gmail.com>
2021-09-15 00:30:49 +0530

    -- signup eith google

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { SocialLoginModule, SocialAuthServiceConfig, FacebookLoginProvider } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 
 @NgModule({
   declarations: [],
@@ -11,7 +13,30 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    NgxSpinnerModule
-  ]
+    NgxSpinnerModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '221831887863-9u9p27n9kq7eupp9a6l2rvicd33can2s.apps.googleusercontent.com'
+            )
+          },
+          // {
+          //   id: FacebookLoginProvider.PROVIDER_ID,
+          //   provider: new FacebookLoginProvider(
+          //     '221831887863-9u9p27n9kq7eupp9a6l2rvicd33can2s.apps.googleusercontent.com'
+          //   )
+          // }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
 })
 export class AuthModule { }
--- src/app/auth/candidate/sign-in/sign-in.component.ts
+++ src/app/auth/candidate/sign-in/sign-in.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { SocialAuthService, GoogleLoginProvider, SocialUser, FacebookLoginProvider } from 'angularx-social-login';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -14,18 +15,47 @@ export class SignInComponent implements OnInit {
   form: FormGroup;
   loading = false;
   submitted = false;
-
+  socialUser: SocialUser;
+  isLoggedin: boolean;
   constructor(
     private formBuilder: FormBuilder,
     private _router: Router,
     private _authService: AuthService,
-    private _toastrService: ToastrService, private SpinnerService: NgxSpinnerService
+    private _toastrService: ToastrService, private SpinnerService: NgxSpinnerService, private socialAuthService: SocialAuthService
   ) { }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = (user != null);
+      console.log(this.socialUser);
+      const json = {};
+      json['first_name'] = this.socialUser.firstName;
+      json['last_name'] = this.socialUser.lastName;
+      json['email'] = this.socialUser.email;
+      // json['phone'] = this.form.controls.phone.value;
+      // json['specialization'] = this.form.controls.specialization.value;
+      // json['password'] = this.form.controls.password.value;
+      this._authService.candidateRegister(json).subscribe(response => {
+        if (response.result !== 'fail') {
+          this.submitted = false;
+          // sessionStorage.setItem('_ud', JSON.stringify(response.data))
+          this._router.navigate(['/auth/candidate/sigin'])
+          this.form.reset();
+          this._toastrService.success(
+            'User Registered successfully', response.result,
+            { toastClass: 'toast ngx-toastr', closeButton: true }
+          );
+        } else {
+          this._toastrService.error(
+            response.message, response.result
+          )
+        }
+      })
     });
   }
 
@@ -57,4 +87,14 @@ export class SignInComponent implements OnInit {
       })
     }
   }
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  loginInWithFB(): void {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then(x => console.log(x));
+  }
+
+  // signInWithLinkedIn(): void {
+  //   this.socialAuthService.signIn(LinkedInLoginProvider.PROVIDER_ID).then(x => console.log(x));
+  // }
 }
