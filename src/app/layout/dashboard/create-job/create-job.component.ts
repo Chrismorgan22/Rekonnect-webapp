eeaf223480ba5fdebe4320d4d0f74aaae9ffcc63
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-12-04 15:36:20 +0530

    done sending data to backend for posting job

--- src/app/layout/dashboard/create-job/create-job.component.ts
+++ src/app/layout/dashboard/create-job/create-job.component.ts
@@ -1,16 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JobService } from '../../../services/job.service';
 @Component({
   selector: 'app-create-job',
   templateUrl: './create-job.component.html',
-  styleUrls: ['./create-job.component.scss']
+  styleUrls: ['./create-job.component.scss'],
 })
 export class CreateJobComponent implements OnInit {
+  constructor(private applyJob: JobService) {}
 
-  constructor() { }
+  jobDetails: {
+    Title: string;
+    Type: string;
+    Category: string;
+    City: string;
+    Country: string;
+    minSalary: string;
+    maxSalary: string;
+    minExp: string;
+    maxExp: string;
+  } = {
+    Title: '',
+    Type: '',
+    Category: '',
+    City: '',
+    Country: '',
+    minSalary: '',
+    maxSalary: '',
+    minExp: '',
+    maxExp: '',
+  };
 
+  handleTitle = (event: Event) => {
+    this.jobDetails.Title = (<HTMLInputElement>event.target).value;
+  };
+  handleType = (value: string) => {
+    console.log(value);
+
+    this.jobDetails.Type = value;
+  };
+  handleCat = (event: Event) => {
+    this.jobDetails.Category = (<HTMLInputElement>event.target).value;
+  };
+  handleCity = (event: Event) => {
+    this.jobDetails.City = (<HTMLInputElement>event.target).value;
+  };
+  handleCountry = (event: Event) => {
+    this.jobDetails.Country = (<HTMLInputElement>event.target).value;
+  };
+  handleMinS = (event: Event) => {
+    this.jobDetails.minSalary = (<HTMLInputElement>event.target).value;
+  };
+  handleMaxS = (event: Event) => {
+    this.jobDetails.maxSalary = (<HTMLInputElement>event.target).value;
+  };
+  handleMaxE = (event: Event) => {
+    this.jobDetails.maxExp = (<HTMLInputElement>event.target).value;
+  };
+  handleMinE = (event: Event) => {
+    this.jobDetails.minExp = (<HTMLInputElement>event.target).value;
+  };
+
+  handleSubmit = () => {
+    console.log(this.jobDetails);
+    this.applyJob.postJobs(this.jobDetails).subscribe(() => {
+      console.log('Job created!!');
+    });
+  };
   ngOnInit(): void {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }
-
 }
