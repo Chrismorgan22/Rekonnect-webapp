77d6773401111d4174df7d3f50329285329c3fff
Leena <keshavnodejs01@gmail.com>
2021-12-10 15:13:45 +0530

    -- job service chnage

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
+import { environment } from '../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class JobService {
-  private _url: string = 'http://localhost:8000';
   constructor(private http: HttpClient) { }
 
   getJobs() {
-    return this.http.post<any>(`${this._url}/job/list`, null).pipe(
+    return this.http.post<any>(`${environment.apiUrl}/job/list`, null).pipe(
       map((response) => {
         return response;
       })
@@ -16,7 +15,7 @@ export class JobService {
   }
   applyJob(jobData) {
     return this.http
-      .post<any>(`${this._url}/job/application/apply`, jobData)
+      .post<any>(`${environment.apiUrl}/job/application/apply`, jobData)
       .pipe(
         map((response) => {
           return response;
@@ -24,21 +23,21 @@ export class JobService {
       );
   }
   postJobs(job: any) {
-    return this.http.post<any>(`${this._url}/job/save`, job).pipe(
+    return this.http.post<any>(`${environment.apiUrl}/job/save`, job).pipe(
       map((response) => {
         return response;
       })
     );
   }
   getJobDetails(id) {
-    return this.http.get<any>(`${this._url}/job/details/` + id).pipe(
+    return this.http.get<any>(`${environment.apiUrl}/job/details/` + id).pipe(
       map((response) => {
         return response;
       })
     );
   }
   getJobAppliedStatus(body) {
-    return this.http.post<any>(`${this._url}/job/application/applied/status`, body).pipe(
+    return this.http.post<any>(`${environment.apiUrl}/job/application/applied/status`, body).pipe(
       map((response) => {
         return response;
       })
