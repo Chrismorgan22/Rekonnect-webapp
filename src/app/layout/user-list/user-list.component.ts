7410eedf7f8858ec0882ab10b3c79b425d03d0e3
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-12-11 14:17:54 +0530

    done with search func

--- src/app/layout/user-list/user-list.component.ts
+++ src/app/layout/user-list/user-list.component.ts
@@ -2,32 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { LayoutService } from 'src/app/services/layout.service';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
-  userData: any;
-  constructor(public _layoutService: LayoutService, public _toastrService: ToastrService, public SpinnerService: NgxSpinnerService) { }
-
+  userData: any[];
+  searchTerm: string;
+  constructor(
+    public _layoutService: LayoutService,
+    public _toastrService: ToastrService,
+    public SpinnerService: NgxSpinnerService
+  ) {}
+  filterUser: string[];
   ngOnInit(): void {
     this.getUserListData(1);
+  }
+
+  filterUsers(event: Event): void {
+    this.searchTerm = (<HTMLInputElement>event.target).value;
+    console.log(this.searchTerm);
+    console.log(typeof this.searchTerm);
+    console.log(this.userData[0]);
+
+    this.filterUser = this.userData.filter((user) => {
+      return user.user_details.first_name.search(this.searchTerm) != -1;
+    });
   }
   getUserListData(userRole) {
     this.userData = [];
     this.SpinnerService.show();
-    this._layoutService.getUserList(userRole).subscribe(response => {
+    this._layoutService.getUserList(userRole).subscribe((response) => {
       this.SpinnerService.hide();
       if (response.result !== 'fail') {
         this.userData = response.data;
+        this.filterUser = this.userData;
       } else {
         this._toastrService.error(
-          'User list is not available', 'No Data Found'
-        )
+          'User list is not available',
+          'No Data Found'
+        );
       }
-    })
+    });
   }
   changeUserRoleSwitch(event) {
     console.log(event, event.target.checked);
