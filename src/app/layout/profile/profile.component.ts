4b9293644584f72eb205bf4489bca2ff921d00c6
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-01-20 18:35:18 +0530

    fetched userData from db

--- src/app/layout/profile/profile.component.ts
+++ src/app/layout/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JobService } from '../../services/job.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -9,11 +9,20 @@ export class ProfileComponent implements OnInit {
   userId: string = JSON.parse(sessionStorage.getItem('_ud'))[0]._id;
   professionalData: {};
   userData: any = JSON.parse(sessionStorage.getItem('_ud'))[0];
-
-  constructor() {
+  isfirst: boolean = false;
+  personalData: any = {};
+  constructor(private _jobService: JobService) {
     console.log(this.userData);
   }
 
+  toggleFirstEdit() {
+    this.isfirst = !this.isfirst;
+  }
   updateField() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._jobService.fetchCandidate(this.userId).subscribe((data) => {
+      this.personalData = data;
+      console.log(data);
+    });
+  }
 }
