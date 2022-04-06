c3e77e2c9fc5b54c5f8961a2377996fd39205e9f
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-06 10:12:01 +0530

    added a toggle button for inactive/active job

--- src/app/layout/dashboard/employer-view-job/employer-view-job.component.ts
+++ src/app/layout/dashboard/employer-view-job/employer-view-job.component.ts
@@ -42,7 +42,17 @@ export class EmployerViewJobComponent implements OnInit {
       this.employerInfo = data[0];
     });
   }
+  toggleStatus(id: string) {
+    // console.log(this.jobData);
+    var json = this.jobData.filter((jb) => jb._id == id);
+    console.log(json);
+    json[0].status = !json[0].status;
+    console.log(json);
 
+    this.jobService.updateJob(id, json[0]).subscribe((res) => {
+      console.log(res);
+    });
+  }
   getUserProfileData() {
     const localData = JSON.parse(sessionStorage.getItem('_ud'))[0];
     this.layoutService.getUserProfile(localData._id).subscribe((res) => {
@@ -51,6 +61,7 @@ export class EmployerViewJobComponent implements OnInit {
       // this.empID = this.userProfileData;
       this.empID = this.userProfileData.employer_details[0]._id;
       console.log(this.userProfileData);
+      // window.location.reload();
     });
   }
 
