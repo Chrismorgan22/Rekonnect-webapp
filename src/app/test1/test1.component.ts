21af37e08435d11510c68c5141a3ef3105d5ab0e
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-13 12:03:38 +0530

    fetched dynamic data from backend

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
-  styleUrls: ['./test1.component.scss']
+  styleUrls: ['./test1.component.scss'],
 })
 export class Test1Component implements OnInit {
-
-  constructor() { }
+  userData: any = JSON.parse(sessionStorage.getItem('_ud'));
+  mentorData: any;
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.userData == null) {
+      window.location.replace('/');
+      alert('You are not logged in!');
+    }
+    this.fetchData();
   }
-
+  async fetchData() {
+    const res = await (
+      await fetch(`${environment.apiUrl}/mentor/fetch/${this.userData[0]._id}`)
+    ).json();
+    this.mentorData = res;
+  }
 }
