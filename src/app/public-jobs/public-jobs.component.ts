612262ed94069091f53d54fbe36cfb379ddefaa8
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-13 12:57:58 +0530

    displayed jobs in the compoeent

--- src/app/public-jobs/public-jobs.component.ts
+++ src/app/public-jobs/public-jobs.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JobService } from '../services/job.service';
 @Component({
   selector: 'app-public-jobs',
   templateUrl: './public-jobs.component.html',
-  styleUrls: ['./public-jobs.component.scss']
+  styleUrls: ['./public-jobs.component.scss'],
 })
 export class PublicJobsComponent implements OnInit {
-
-  constructor() { }
+  entireJobDetails: any[];
+  constructor(private _jobService: JobService) {}
 
   ngOnInit(): void {
+    this._jobService.getAllJobs().subscribe((res) => {
+      console.log(res);
+
+      this.entireJobDetails = res;
+    });
   }
 
+  applyJob(jobId: any) {
+    if (sessionStorage.getItem('_ud') == null) {
+      alert('You are not logged in,Please Login in to apply for a job');
+      return;
+    }
+    window.location.replace(`/dashboard/apply-for-job/${jobId}`);
+  }
 }
