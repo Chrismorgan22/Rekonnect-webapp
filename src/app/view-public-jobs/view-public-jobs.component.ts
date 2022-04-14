720eccea6e94d8b1c67cd9d626adfedf1ac923ca
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-14 11:03:13 +0530

    fetched job data from backend

--- src/app/view-public-jobs/view-public-jobs.component.ts
+++ src/app/view-public-jobs/view-public-jobs.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JobService } from '../services/job.service';
 @Component({
   selector: 'app-view-public-jobs',
   templateUrl: './view-public-jobs.component.html',
-  styleUrls: ['./view-public-jobs.component.scss']
+  styleUrls: ['./view-public-jobs.component.scss'],
 })
 export class ViewPublicJobsComponent implements OnInit {
+  jobDetails: any;
+  jobId = window.location.pathname.split('/')[3];
 
-  constructor() { }
+  constructor(private _jobService: JobService) {}
 
   ngOnInit(): void {
+    this._jobService.getJobDetails(this.jobId).subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
