ce27ba38c0b86822386030a652831a896d6bedcf
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-20 15:07:41 +0530

    directed user to apply page

--- src/app/view-public-jobs/view-public-jobs.component.ts
+++ src/app/view-public-jobs/view-public-jobs.component.ts
@@ -9,6 +9,7 @@ export class ViewPublicJobsComponent implements OnInit {
   jobDetails: any;
   jobId = window.location.pathname.split('/')[3];
   companyData: any;
+  userData: any = JSON.parse(sessionStorage.getItem('_ud'));
   constructor(private _jobService: JobService) {}
 
   ngOnInit(): void {
@@ -17,5 +18,14 @@ export class ViewPublicJobsComponent implements OnInit {
       this.jobDetails = data.data;
       this.companyData = { logo: data.company_logo, name: data.company_name };
     });
+  }
+  applyJob() {
+    if (this.userData == null) {
+      alert('ypou are not logged in');
+      window.location.replace('/');
+    }
+    console.log(this.jobDetails._id);
+
+    window.location.replace(`/dashboard/apply-for-job/${this.jobDetails._id}`);
   }
 }
